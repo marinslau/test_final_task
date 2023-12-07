dc490ebb8c3997340031c83f8714bdf333ef3edf
marinslau <marinslautina@gmail.com>
2023-12-07 12:37:14 +0500

    rewrited test in Page Object

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,10 +1,9 @@
 class BasePage():
 
     # конструктор — метод, который вызывается, когда мы создаем объект
-	def __init__(self, browser, url):
+    def __init__(self, browser, url):
         self.browser = browser
         self.url = url
 
-    # метод открывает нужную страницу, используя метод get()
     def open(self):
-    	self.browser.get(self.url)
+        self.browser.get(self.url)
