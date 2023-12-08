829a39a197f7fbbff4a3af781ff8c884da06c5ef
marinslau <marinslautina@gmail.com>
2023-12-08 16:52:22 +0500

    test with login_page

--- test_main_page.py
+++ test_main_page.py
@@ -9,6 +9,5 @@ def test_guest_can_go_to_login_page(browser):
     page.should_be_login_link()
     page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
     
-    
-    page = LoginPage(browser, browser.current_url)   # переинициализируем
-    page.should_be_login_page()
+    login_page = LoginPage(browser, browser.current_url)   # инициализируем LoginPage
+    login_page.should_be_login_page()
